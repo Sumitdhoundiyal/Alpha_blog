96584b2c268e8317363d03fb73cb9bc20bb05b9a
Sumit Dhoundiyal <your-sumit.dhoundiyal.56@gmail.com>
2025-07-17 13:51:26 +0530

    ui restriction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+before_action :set_user, only:[:show,:edit,:update]
     def index
       @users = User.paginate(page: params[:page], per_page: 5)
 
@@ -10,16 +10,14 @@ class UsersController < ApplicationController
     end
 
     def show
-      @user=User.find(params[:id])
       @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def edit
-      @user =User.find(params[:id])
     end
 
     def update
-      @user =User.find(params[:id])
+
       if @user.update(user_params)
         flash[:notice] = "Your user Update Successfully"
         redirect_to @user
@@ -31,6 +29,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+          session[:user_id] = @user.id
           flash[:notice] = "Welcome to the Alpha Blog,you have successfully sign-up"
           redirect_to articles_path
 
@@ -46,4 +45,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username,:email,:password)
   end
 
+  def set_user
+    @user =User.find(params[:id])
+  end
 end
